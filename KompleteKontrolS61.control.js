348c9228792ba8fe4bf28763ce8b7c6fd0c476d1
Thomas Helzle <ThomasHelzle@users.noreply.github.com>
2014-11-15 14:25:23 +0100

    Fixed Error in Autodetection for Mac OSX

--- KompleteKontrolS61.control.js
+++ KompleteKontrolS61.control.js
@@ -6,4 +6,4 @@ load ("KompleteKontrol.js");
 
 host.defineController("Native Instruments", "Komplete Kontrol S 61", "1.0", "ea3c2e00-69bf-11e4-9803-0800200c9a66");
 host.defineMidiPorts(3, 3);
-host.addDeviceNameBasedDiscoveryPair(["KOMPLETE KONTROL S61 Port 1", "KOMPLETE KONTROL S61 Port 1", "Komplete Kontrol DAW - 1"], ["KOMPLETE KONTROL S61 Port 1", "KOMPLETE KONTROL S61 Port 1", "Komplete Kontrol DAW - 1"]);
+host.addDeviceNameBasedDiscoveryPair(["KOMPLETE KONTROL S61 Port 1", "KOMPLETE KONTROL S61 Port 2", "Komplete Kontrol DAW - 1"], ["KOMPLETE KONTROL S61 Port 1", "KOMPLETE KONTROL S61 Port 2", "Komplete Kontrol DAW - 1"]);
